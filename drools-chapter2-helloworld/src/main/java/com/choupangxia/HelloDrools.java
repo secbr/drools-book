4474a9df33fe06a9f6a90afd26d33b42bb5b3057
secbro <xinyoulingxi2008@126.com>
2021-01-17 21:37:36 +0800

    add hello world

---
+++ drools-chapter2-helloworld/src/main/java/com/choupangxia/HelloDrools.java
@@ -0,0 +1,24 @@
+package com.choupangxia;
+
+import com.choupangxia.entity.Person;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2021/1/17
+ **/
+public class HelloDrools {
+
+	public static void main(String[] args) {
+		KieServices kieServices = KieServices.Factory.get();
+		KieContainer kieContainer = kieServices.getKieClasspathContainer();
+		KieSession kieSession = kieContainer.newKieSession();
+		Person p = new Person();
+		kieSession.insert(p);
+		int count = kieSession.fireAllRules();
+		System.out.println("触发了" + count + "规则。");
+	}
+}
---
+++ drools-chapter2-helloworld/src/main/java/com/choupangxia/entity/Person.java
@@ -0,0 +1,29 @@
+package com.choupangxia.entity;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2021/1/17
+ **/
+public class Person {
+
+	private String username;
+
+	private int age;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
