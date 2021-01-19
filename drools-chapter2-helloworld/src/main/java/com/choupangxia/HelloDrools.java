b7ab0ad807b927ec5b1f8ec06de621ee71275dbb
secbro <xinyoulingxi2008@126.com>
2021-01-19 19:24:33 +0800

    编写规则代码

--- drools-chapter2-helloworld/src/main/java/com/choupangxia/HelloDrools.java
+++ drools-chapter2-helloworld/src/main/java/com/choupangxia/HelloDrools.java
@@ -12,13 +12,18 @@ import org.kie.api.runtime.KieSession;
  **/
 public class HelloDrools {
 
-	public static void main(String[] args) {
-		KieServices kieServices = KieServices.Factory.get();
-		KieContainer kieContainer = kieServices.getKieClasspathContainer();
-		KieSession kieSession = kieContainer.newKieSession();
-		PersonFact p = new PersonFact();
-		kieSession.insert(p);
-		int count = kieSession.fireAllRules();
-		System.out.println("触发了" + count + "规则。");
-	}
+    public static void main(String[] args) {
+        // 生产中只需将代码部分进行包装，BRMS系统更多的是将代码部分与规则文件部分进行剥离和封装。
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
+        PersonFact p = new PersonFact();
+        p.setAge(17);
+        kieSession.insert(p);
+        int count = kieSession.fireAllRules();
+        System.out.println("触发了" + count + "条规则。");
+        // PersonFact对象在WorkingMemory中以引用的形式传递，规则引擎不会创建新的对象，
+        // 因此可通过引用直接获得修改之后的值。
+        System.out.println("规则执行结果，score=" + p.getScore() + ",desc=" + p.getDesc() + "。");
+    }
 }
