33b49bdc8560e22eb45b170029e0dc782e1aa1a8
secbro <xinyoulingxi2008@126.com>
2021-03-13 11:38:42 +0800

    完善

---
+++ drools-chapter3-score/src/main/java/com/choupangxia/StaticMethodImportTest.java
@@ -0,0 +1,28 @@
+package com.choupangxia;
+
+import com.choupangxia.entity.Person;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+/**
+ * FactHandler使用演示
+ *
+ * @author sec
+ * @version 1.0
+ **/
+public class StaticMethodImportTest {
+
+	public static void main(String[] args) {
+		KieServices kieServices = KieServices.get();
+		KieContainer kieContainer = kieServices.getKieClasspathContainer();
+		KieSession kieSession = kieContainer.newKieSession("static-method");
+
+		Person p = new Person();
+		p.setAge(17);
+		kieSession.insert(p);
+		int count = kieSession.fireAllRules();
+		System.out.println("触发了" + count + "条规则。");
+	}
+}
---
+++ drools-chapter3-score/src/main/java/com/choupangxia/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.choupangxia.utils;
+
+/**
+ * @author sec
+ * @version 1.0
+ * @date 2021/3/13
+ **/
+public class StringUtils {
+
+	public static boolean isEmpty(String s){
+		return s == null || "".equals(s);
+	}
+}
